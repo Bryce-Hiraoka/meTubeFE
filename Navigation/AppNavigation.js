606448a62d8011fc91f1a724a3fab8bc3dc31d5e
YuyusanK <kurinoyuichi@gmail.com>
2020-08-12 18:09:03 -0700

    added all of the module. compiles and runs now.

---
+++ Navigation/AppNavigation.js
@@ -0,0 +1,40 @@
+import {createStackNavigator} from "react-navigation-stack";
+import {createBottomTabNavigator} from "react-navigation-tabs";
+import {
+    createAppContainer,
+    createSwitchNavigator
+} from "react-navigation";
+import {Alert} from "react-native";
+import {FontAwesome} from "react-native-vector-icons";
+import React from 'react';
+
+import SignIn from "../screens/SignIn";
+import SignUp from "../screens/SignUp";
+
+export const notLoggedIn = createStackNavigator(
+    {
+        SignIn:{
+            screen:SignIn,
+            navigationOptions: {
+                headerTitle: 'Signin',
+            }
+        },
+        SignUp:{
+            screen:SignUp,
+            navigationOptions: {
+                headerTitle: 'SignUp',
+            }
+        }
+    }
+);
+
+
+export default createAppContainer(
+    createSwitchNavigator(
+        {
+            notLoggedIn:{
+                screen:notLoggedIn
+            }
+        }
+    )
+);
