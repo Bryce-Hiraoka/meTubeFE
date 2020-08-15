1302177b00b66a53791943d94062eb77cc03daa3
Bryce Hiraoka <brycehiraoka@Bryces-MacBook-Pro.local>
2020-08-14 17:59:03 -0700

    css for signup

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import {Avatar} from 'react-native-paper';
+import styles from '../Constants/SignInStyle';
 
-export default class Signin extends React.Component{
+export default class SignUp extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            emailFocus:false,
+            numberFocus:false,
             passwordFocus:false,
-            emailstr:'',
-            passwordstr:''
+            confirmPassFocus:false,
+            numberStr:'',
+            passwordStr:''
         }
     }
 
@@ -19,29 +22,79 @@ export default class Signin extends React.Component{
 
     };
 
-    handleEmailFocus = ()=>{
-        this.setState({emailFocus:true})
+    handleNumberFocus = ()=>{
+        this.setState({numberFocus:true})
     };
-    handleEmailBlur = ()=>{
-        this.setState({emailFocus:false})
+
+    handleNumberBlur = ()=>{
+        this.setState({numberFocus:false})
     };
-    handleEmailChange = (event)=>{
-        this.setState({emailstr:event.nativeEvent.text});
+
+    handleNumberChange = (event)=>{
+        this.setState({numberStr:event.nativeEvent.text});
     };
 
     handlePasswordFocus = ()=>{
         this.setState({passwordFocus:true})
     };
+
     handlePasswordBlur = ()=>{
         this.setState({passwordFocus:false})
     };
-    handlePasswordChange = (event)=>{
-        this.setState({passwordstr:event.nativeEvent.text});
+
+    handleConfirmFocus = ()=>{
+        this.setState({confirmPassFocus:true})
     };
+
+    handleConfirmBlur = ()=>{
+        this.setState({confirmPassFocus:false})
+    };
+
+    handlePasswordChange = (event)=>{
+        this.setState({passwordStr:event.nativeEvent.text});
+    };
+
     render(){
         return(
-            <View>
-
+            <View style={styles.container}>
+                <Avatar.Image style={styles.logo}
+                              size={200}
+                              source={require('../assets/PVlogo.jpeg')}
+                />
+                <View style={styles.box}>
+                    <TextInput
+                        style={this.state.numberFocus?styles.textInputHoverNum:styles.textInputNum}
+                        onFocus={this.handleNumberFocus}
+                        onBlur={this.handleNumberBlur}
+                        placeholderTextColor='black'
+                        placeholder="Number"
+                        onChange={this.handleNumberChange}
+                    />
+                    <TextInput
+                        style={this.state.passwordFocus?styles.textInputHoverPass:styles.textInputPass}
+                        onFocus={this.handlePasswordFocus}
+                        onBlur={this.handlePasswordBlur}
+                        placeholderTextColor='black'
+                        placeholder="Password"
+                        onChange={this.handlePasswordChange}
+                    />
+                    <TextInput
+                        style={this.state.confirmPassFocus?styles.textInputHoverPass:styles.textInputPass}
+                        onFocus={this.handleConfirmFocus}
+                        onBlur={this.handleConfirmBlur}
+                        placeholderTextColor='black'
+                        placeholder="Confirm Password"
+                    />
+                    <TouchableOpacity
+                        onPress={this.handleSubmit}
+                        style={styles.button}
+                    >
+                        <Text
+                            style={styles.buttonText}>
+                            Login
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
     );
     }
